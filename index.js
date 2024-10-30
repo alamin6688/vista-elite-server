fa321cea9776272a821a0ad5a423b91ccd9ae827
alamin6688 <alamin.kzs06@gmail.com>
2024-10-30 19:32:52 +0600

    Get all rooms API integrated

--- index.js
+++ index.js
@@ -53,7 +53,9 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // auth related api
+    const roomsCollecton = client.db("vistaElite").collection("rooms");
+
+    // Auth related api
     app.post("/jwt", async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
@@ -81,6 +83,12 @@ async function run() {
       } catch (err) {
         res.status(500).send(err);
       }
+    });
+
+    // Get all rooms from DB
+    app.get("/rooms", async (req, res) => {
+      const result = await roomsCollecton.find().toArray();
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
